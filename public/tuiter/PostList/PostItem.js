088dd4614396cd3a725c9cc0314b5406d60c71ee
mitug <mitughotgalkar@gmail.com>
2022-10-16 18:16:32 -0400

    Home screen final touches

--- public/tuiter/PostList/PostItem.js
+++ public/tuiter/PostList/PostItem.js
@@ -5,7 +5,7 @@ const PostItem = (post) => {
                 <img class="wd-avatar" src="${post.image}"></div>
             <div class="wd-right-portion">
                 <div>
-                    <span class="wd-author-name">${post.userName}</span>
+                    <span class="wd-author-name">${post.name}</span>
                     <i class="fas fa-check-circle ml-2"></i>
                     <span class="wd-author-tag">${post.tag}</span>
                     <span class="wd-author-tag dot-before">. ${post.time}</span>
@@ -14,16 +14,15 @@ const PostItem = (post) => {
                             <i class="fas fa-ellipsis-h"></i>
                         </span>
                     </a>
-                    <span class="wd-content wd-row">${post.topic1} <a href="${post.externalLink}">${post.tagInTitle}</a> ${post.topic2}</span>
+                    <span class="wd-content wd-row">${post.mainTopic}
+                    <a href="${post.hyperlink}">${post.hyperlinkTag}</a> ${post.blueLink}</span>
                 </div>
                 <div class="wd-item-body">
-                    <img src="${post.bannerUrl}" class="wd-item-banner">
-                    <span class="wd-body-title wd-row ${post.overlayTextPresent === 'false' ? "d-none" : ""}">${post.overlayTextHeading}</span>
-                    <span class="wd-body-content wd-row ${post.overlayTextPresent === 'false' ? "d-none" : ""}">${post.overlayText}</span>
-                    <span class="wd-body-content wd-row ${post.overlayTextPresent === 'false' ? "d-none" : ""}">
-                        <a class="text-secondary wd-no-decoration" href="${post.externalLink}">
-                            <i class="fas fa-link"></i>
-                            ${post.overlayLinkText}
+                    <img src="${post.bigImage}" class="wd-item-banner">
+                    <span class="wd-body-title wd-row ${post.extraText === 'false' ? "d-none" : ""}">${post.bigHeading}</span>
+                    <span class="wd-body-content wd-row ${post.extraText === 'false' ? "d-none" : ""}">${post.bigText}</span>
+                    <span class="wd-body-content wd-row ${post.extraText === 'false' ? "d-none" : ""}">
+                        <a class="text-secondary wd-no-decoration" href="${post.hyperlink}"><i class="fas fa-link"></i>${post.pinText}
                         </a>
                     </span>
                 </div>
@@ -31,39 +30,32 @@ const PostItem = (post) => {
                     <ul class="wd-reaction">
                         <li>
                             <a href="#!" class="wd-no-decoration text-secondary">
-                                <span class="">
-                                    <i class="far fa-comment"></i>
+                                <span class=""><i class="far fa-comment"></i>
                                 </span>
                                 <span class="wd-reaction-count">${post.commentCount}</span>
                             </a>
                         </li>
                         <li>
                             <a href="#!" class="wd-no-decoration text-secondary">
-                            <span class="">
-                                <i class="fas fa-retweet"></i>
+                            <span class=""><i class="fas fa-retweet"></i>
                             </span>
                                 <span class="wd-reaction-count">${post.retweetCount}</span>
                             </a>
                         </li>
                         <li>
                             <a href="#!" class="wd-no-decoration text-secondary">
-                            <span class="wd-reaction-icon">
-                                <i class="fas fa-heart reaction-active"></i>
+                            <span class="wd-reaction-icon"><i class="fas fa-heart reaction-active"></i>
                             </span>
                                 <span class="wd-reaction-count reaction-active">${post.likeCount}</span>
                             </a>
                         </li>
                         <li>
                             <a href="#!" class="wd-no-decoration text-secondary">
-                            <span class="wd-reaction-icon">
-                                <i class="fas fa-upload"></i>
+                            <span class="wd-reaction-icon"><i class="fas fa-upload"></i>
                             </span>
                             </a>
                         </li>
-                    </ul>
-
-                </div>
-            </div>
+                    </ul> </div> </div>
         </li>
     `);
 }
