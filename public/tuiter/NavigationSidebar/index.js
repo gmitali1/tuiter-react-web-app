b535401b9e5b2aa8f20cef2714a8f30d7365278b
mitug <mitughotgalkar@gmail.com>
2022-10-23 15:33:10 -0400

    deleted A5 tuiter folder for avoiding redirects

--- public/tuiter/NavigationSidebar/index.js
+++
@@ -1,7 +0,0 @@
-import navigationsidebars from "./navigationsidebars.js";
-import NavigationItem from "./NavigationItem.js";
-
-const NavigationSidebar = (active) => `
-        <div class="list-group">${navigationsidebars.map(navigationItem => NavigationItem(navigationItem, active)).join('')}</div>
-        <button class="btn btn-primary w-100 mt-2 rounded-pill">Tuit</button>`
-export default NavigationSidebar;
